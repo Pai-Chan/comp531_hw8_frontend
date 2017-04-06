b293787a4f7a61a69ad770cc45508eb080c61f53
Pei Chen <pai@Peis-MacBook-Pro.local>
2017-04-06 13:22:41 -0500

    update readme.json

--- src/actions.js
+++ src/actions.js
@@ -27,7 +27,7 @@ export function getAction(type, otherStatesObj) {
 // for this assignment, we don't make http calls,
 // so the effective statements are commented.
 
-export const url = 'https://webdev-dummy.herokuapp.com'
+export const url = 'http://127.0.0.1:3000'
 
 export function resource(method, endpoint, payload, notJson){
 
